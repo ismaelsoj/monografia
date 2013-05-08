f6422328e1eeb7037e4b1b9b87b0b965ae8f5fcc
Ismael de Souza Oliveira Júnior <ismaelsoj@gmail.com>
2013-05-07 22:56:37 -0300

    Primeira entrada dos dois projetos. Um é o cliente de Web service feito com o CXF e o outro é um projeto contendo classes comuns a serviços e clientes

---
+++ clientecxf/src/main/java/br/com/ismael/ClienteCXF.java
@@ -0,0 +1,124 @@
+package br.com.ismael;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.dynamic.DynamicClientFactory;
+
+/**
+ * Classe que invoca um método de um web service de forma dinâmica através de um WSDL previamente publicado.
+ * 
+ * @author Ismael
+ * 
+ */
+public class ClienteCXF {
+
+  private JFrame frame;
+  private JPanel panel;
+  private JPanel panelTimes;
+  private JPanel panelResultado;
+  private JLabel labelTime1;
+  private JLabel labelTime2;
+  private JTextField fieldTime1;
+  private JTextField fieldTime2;
+  private JLabel labelCidade;
+  private JTextField fieldCidade;
+  private JButton consultar;
+  private JButton cancelar;
+
+  public ClienteCXF() {
+    this.frame = new JFrame("Consulta de jogos");
+    this.panel = new JPanel();
+    this.panelTimes = new JPanel();
+    this.panelResultado = new JPanel();
+    this.labelTime1 = new JLabel("Informe o time 1: ");
+    this.fieldTime1 = new JTextField(30);
+    this.labelTime2 = new JLabel("Informe o time 2: ");
+    this.fieldTime2 = new JTextField(30);
+    this.labelCidade = new JLabel("Cidade (opcional): ");
+    this.fieldCidade = new JTextField(30);
+    this.consultar = new JButton("Consultar Partida");
+    this.cancelar = new JButton("Cancelar");
+  }
+
+  public void exibeInterfaceGrafica() {
+    panel.setLayout(new BorderLayout());
+    panelTimes.setLayout(new GridLayout(4, 2));
+    panelTimes.add(labelTime1);
+    panelTimes.add(fieldTime1);
+    panelTimes.add(labelTime2);
+    panelTimes.add(fieldTime2);
+    panelTimes.add(labelCidade);
+    panelTimes.add(fieldCidade);
+    consultar.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        /*
+         * Instancia o objeto que será responsável por criar os clientes dinamicamente
+         */
+        DynamicClientFactory factory = DynamicClientFactory.newInstance();
+        // Cria o cliente através do endereço do WSDL
+        Client client = factory.createClient("http://localhost:8080/webservicecxf/ConsultaJogo?wsdl");
+        try {
+          /*
+           * Invoca o método através do nome e informa os parâmetros necessários para a execução do mesmo
+           */
+          Object[] obj = client.invoke("consultaJogo", "Galo", "Cruzeiro", null);
+          for (Object object : obj) {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("Time 1: " + ((Jogo) object).getTime1());
+            buffer.append("\n");
+            buffer.append("Time 2: " + ((Jogo) object).getTime2());
+            buffer.append("\n");
+            buffer.append("Resultado: " + ((Jogo) object).getResultado());
+            buffer.append("\n");
+            buffer.append("Cidade: " + ((Jogo) object).getCidade());
+            buffer.append("\n");
+            buffer.append("Motivo: " + ((Jogo) object).getMotivo());
+            buffer.append("\n");
+            buffer.append("Informações Adicionais: " + ((Jogo) object).getInformacoesAdicionais());
+            JOptionPane.showMessageDialog(null, buffer.toString(), "RESULTADO DA CONSULTA",
+                JOptionPane.INFORMATION_MESSAGE);
+            frame.pack();
+          }
+        } catch (Exception e1) {
+          e1.printStackTrace();
+        }
+      }
+    });
+    panelTimes.add(consultar);
+    cancelar.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+
+      }
+    });
+    panelTimes.add(cancelar);
+    panel.add(panelTimes, BorderLayout.NORTH);
+    panel.add(panelResultado, BorderLayout.SOUTH);
+    frame.add(panel);
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public static void main(String[] args) {
+
+    ClienteCXF cliente = new ClienteCXF();
+    cliente.exibeInterfaceGrafica();
+
+  }
+
+}
---
+++ comuns/src/main/java/br/com/ismael/Jogo.java
@@ -0,0 +1,87 @@
+package br.com.ismael;
+
+/**
+ * Classe que representa um jogo de futebol.
+ * 
+ * @author Ismael
+ * 
+ */
+public class Jogo {
+
+  private String time1;
+  private String time2;
+  private String resultado;
+  private String cidade;
+  private String motivo;
+  private String informacoesAdicionais;
+
+  public Jogo(String time1, String time2, String resultado, String cidade, String motivo, String informacoesAdicionais) {
+    this.time1 = time1;
+    this.time2 = time2;
+    this.resultado = resultado;
+    this.cidade = cidade;
+    this.motivo = motivo;
+    this.informacoesAdicionais = informacoesAdicionais;
+  }
+
+  public Jogo() {
+  }
+
+  /*
+   * Métodos acessores
+   */
+  public String getTime1() {
+    return time1;
+  }
+
+  public void setTime1(String time1) {
+    this.time1 = time1;
+  }
+
+  public String getTime2() {
+    return time2;
+  }
+
+  public void setTime2(String time2) {
+    this.time2 = time2;
+  }
+
+  public String getResultado() {
+    return resultado;
+  }
+
+  public void setResultado(String resultado) {
+    this.resultado = resultado;
+  }
+
+  public String getCidade() {
+    return cidade;
+  }
+
+  public void setCidade(String cidade) {
+    this.cidade = cidade;
+  }
+
+  public String getMotivo() {
+    return motivo;
+  }
+
+  public void setMotivo(String motivo) {
+    this.motivo = motivo;
+  }
+
+  public String getInformacoesAdicionais() {
+    return informacoesAdicionais;
+  }
+
+  public void setInformacoesAdicionais(String informacoesAdicionais) {
+    this.informacoesAdicionais = informacoesAdicionais;
+  }
+
+  @Override
+  public String toString() {
+    return this.getTime1() + " x " + this.getTime2() + ": " + this.getResultado() + " - " + this.getCidade() + " - "
+        + this.getMotivo() + " - " + this.getInformacoesAdicionais();
+  }
+
+}
